3e67cda89124818702497953c0031257b6368222
giannismarleotech <giannismarleotech@gmail.com>
2026-09-06 11:55:27 +0200

    Add files via upload

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-static";
 import type { MetadataRoute } from "next";
 import { locales, getDictionary, href, type PageKey } from "@/i18n";
 import { site } from "@/config/site";
