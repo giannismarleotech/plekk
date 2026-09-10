949c80ab6dfee118160e88ec865876e0cd05eb47
giannismarleotech <giannismarleotech@gmail.com>
2026-09-10 22:33:17 +0200

    Delete src/app/(marketing)/[lang]/layout.tsx

--- src/app/(marketing)/[lang]/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { notFound } from "next/navigation";
-import { isLocale } from "@/i18n";
-
-export default async function MarketingLayout({ children, params }: LayoutProps<"/[lang]">) {
-  const { lang } = await params;
-  if (!isLocale(lang)) notFound();
-  return <div className="flex-1 flex flex-col marketing" style={{ ["--brand" as string]: "#1ED760", ["--accent" as string]: "#0F7A38" }}>{children}</div>;
-}
